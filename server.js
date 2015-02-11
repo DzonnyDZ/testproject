d688b7dbfca7386ed54b43c9f4f2984af9f45e08
Đonny <dzonny.dz@gmail.com>
2015-02-11 23:28:56 +0100

    8080->80

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ server.post('/photos', passport.authenticate('bearer', { session: false }), app.
 server.get('/', app.hello);
 
 //Logging
-server.listen(8080, function () {
+server.listen(80, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
